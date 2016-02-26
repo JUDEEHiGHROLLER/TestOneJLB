0f4ee1af29a1b40be5ed153b551105ae7b84dba5
judeb <judeb@localhost>
2016-02-26 14:28:21 -0500

    This is the initial commit

---
+++ TestOneJLB/src/edu/saintjoe/cs/judeb/testonejlb/NameSixTimes.java
@@ -0,0 +1,9 @@
+package edu.saintjoe.cs.judeb.testonejlb;
+
+public class NameSixTimes {
+
+	public static void main(String[] args) {
+		System.out.println("Jude Brace Jude Brace Jude Brace Jude Brace Jude Brace Jude Brace");
+	}
+
+}
